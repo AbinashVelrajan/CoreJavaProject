6df57e29530e4cdb2153c036220487e8bcc2fdf6
Abinash <abinash49920@gmail.com>
2024-01-10 17:36:35 +0530

    corejava

---
+++ src/main/java/Employee.java
@@ -0,0 +1,17 @@
+
+public class Employee {
+	
+	public void empName() {
+		System.out.println("Name");
+	}
+	public void empID() {
+		System.out.println("Id");
+	}
+	public void empEmail() {
+		System.out.println("@");
+	}
+	public void empDob() {
+		System.out.println("29");
+	}
+
+}
